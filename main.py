a80c019e9e8ea4ee993abc3bc89941951e03b069
Tomas Ortega <tomasortega.cs@gmail.com>
2021-03-15 03:21:34 -0400

    adding things to do

--- main.py
+++ main.py
@@ -3,6 +3,38 @@ import argparse
 import requests
 import sys
 
+# TODO: get:
+#   length of the url and the domain
+#   # of dots in url, domain, directory and params
+#   # of hyphens in url, domain, directory and params
+#   # of underlines in url, domain, directory and params
+#   # of slashes in url, domain, directory and params
+#   # of question marks in url, domain, directory and params
+#   # of equal signs in url, domain, directory and params
+#   # of @ in url, domain, directory and params
+#   # of + in url, domain, directory and params
+#   # of * in url, domain, directory and params
+#   # of # in url, domain, directory and params
+#   # of $ in url, domain, directory and params
+#   # of % in url, domain, directory and params
+
+# TODO: determine if there is an email present in the url
+# TODO: store the response time
+# TODO: store the domain_spf (whatever that means)
+# TODO: store the asn_ip
+# TODO: store the time_domain_activation
+# TODO: store the time domain expiration
+# TODO: check if the website has a tls/ssl certificate
+# TODO: figure out the number of redirects
+# TODO: find if the site is in the google index
+# TODO: determine if the url has been shortened
+class Website:
+    def __init__(self, url, content, headers):
+        self.url = url
+        self.content = content
+        self.headers = headers
+
+
 def get_website(url):
     print("Fetching website")
     response = requests.get(url)
