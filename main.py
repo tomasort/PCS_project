beb67ca3b6453057df7aabafc7ab8e77052b3f00
readw1 <readwbrown@gmail.com>
2021-03-31 00:04:44 -0400

    update requirements, catching failure to connect

--- main.py
+++ main.py
@@ -3,20 +3,24 @@ import naivebayes
 import rulebased
 import numpy as np
 
+
 print("Please provide URL to test")
 url = input().strip()
 
 if url[:8] != "https://" and url[:7] != "http://":
     url="http://"+url
 
-w = website_feature_extractor.WebsiteFeatureExtractor(url)
-vector = w.get_features()
-todelete = [107,104,16,102,103,4,20,110,10,38,11,108,8,13,106,15,109,9,12,14,21,39,25,24,27,30,28,29,26,23,32,31,34,33,35,22]
-data = np.delete(vector, todelete, 0)
+try:
+    w = website_feature_extractor.WebsiteFeatureExtractor(url)
+    vector = w.get_features()
+    todelete = [107,104,16,102,103,4,20,110,10,38,11,108,8,13,106,15,109,9,12,14,21,39,25,24,27,30,28,29,26,23,32,31,34,33,35,22]
+    data = np.delete(vector, todelete, 0)
 
-model = naivebayes.MachineLearning()
+    model = naivebayes.MachineLearning()
 
-prediction = model.predict([data])[0]
+    prediction = model.predict([data])[0]
+except:
+    prediction = -1
 
 rules = rulebased.RuleBased()
 flags = rules.flags(url)
@@ -29,11 +33,11 @@ for val in flags:
 if(prediction == 0):
     print("Our model predicted that your URL was not a phishing attack")
     if count > numflags/2:
-        print("However, your URL shows " + str(count) + " out of " + str(numflags) + " flags of being a phishing attack. Please use caution when accessing")
+        print("However, your URL shows " + str(count) + " out of " + str(numflags) + " flags of being a phishing attack. Please use caution when accessing.")
     else:
         print("In addition, your URL shows " + str(count) + " out of " + str(numflags) + " flags of being a phishing attack.")
         print("Your URL is unlikely to be a phishing attack")
-else:
+elif (prediction ==1):
     print("Our model predicted that your URL was a phishing attack")
     if count > numflags/2:
         print("In addition, your URL shows " + str(count) + " out of " + str(numflags) + " flags of being a phishing attack.")
@@ -41,3 +45,10 @@ else:
     else:
         print("In addition, your URL shows " + str(count) + " out of " + str(numflags) + " flags of being a phishing attack.")
         print("Although your URL does not show many potential signs of phishing, please use caution when accessing.")
+else:
+    print("Our model was unable to connect to your URL.")
+    if count > numflags/2:
+        print("However, your URL shows " + str(count) + " out of " + str(numflags) + " flags of being a phishing attack. Please use caution when accessing.")
+    else:
+        print("However, your URL shows only" + str(count) + " out of " + str(numflags) + " flags of being a phishing attack.")
+        print("Your URL is unlikely to be a phishing attack.")
