4047fbfac228e8d9069472d818293b3fabf92614
Tomas Ortega <tomasortega.cs@gmail.com>
2021-03-30 21:49:49 -0400

    fixing date things

--- website_feature_extractor.py
+++ website_feature_extractor.py
@@ -1,4 +1,5 @@
 from bs4 import BeautifulSoup
+import datetime
 from collections import Counter
 from urllib.parse import urlparse, parse_qs
 from nslookup import Nslookup
@@ -6,9 +7,13 @@ from ipaddress import ip_address
 from contextlib import redirect_stdout
 from whois import whois
 import requests, sys, re, os, ipwhois, io, tld
+from sklearn.feature_extraction.text import CountVectorizer
+
+
 
 
 class WebsiteFeatureExtractor:
+
     def __init__(self, url):
         parsed_url = urlparse(url)  # The structure of the parsed url is: scheme://netloc/path;parameters?query#fragment
         # Attributes based on the whole url
@@ -34,7 +39,7 @@ class WebsiteFeatureExtractor:
         self.url_length = len(self.url)
         url_email_addresses = self.get_email(self.url)
         self.url_emails = True if url_email_addresses else False
-        self.url_num_emails = len(url_email_addresses)  # TODO: Currently not in use but we could use it later
+        # self.url_num_emails = len(url_email_addresses)
 
         # Attributes based on the domain part of the url
         self.domain = parsed_url.netloc
@@ -60,6 +65,7 @@ class WebsiteFeatureExtractor:
         self.dom_length = len(self.domain)
         self.dom_is_ip = self.is_ipaddress(parsed_url.netloc)
         self.dom_contains_server_or_client = True if "server" in self.domain.lower() or "client" in self.domain.lower() else False
+        self.dom_spf = None
 
         # Attributes based on the directory part of the url
         self.directory = self.get_directory(parsed_url.path)
@@ -85,7 +91,7 @@ class WebsiteFeatureExtractor:
 
         # Attributes based on the file_name part of the url
         self.file_name = self.get_file_name(parsed_url.path)
-        # TODO: we could also test for the file extension
+        # we could also test for the file extension
         count = Counter(self.file_name)
         self.file_num_dot = count['.']
         self.file_num_hyphen = count['-']
@@ -129,7 +135,7 @@ class WebsiteFeatureExtractor:
         self.params_num_doll = count['$']
         self.params_num_perc = count['%']
         self.params_length = len(self.param_string)
-        self.params_tld_present = None;  # TODO: find out if there is a tld in the param_string variable (hard)
+        self.params_tld_present = self.get_tld(self.param_string);  # find out if there is a tld in the param_string variabe
         self.params_nums = len(self.params)
 
         # Attributes based on TLD (top level domain)
@@ -159,26 +165,37 @@ class WebsiteFeatureExtractor:
         ip_record = dns_query.dns_lookup(parsed_url.netloc)  #TODO: time this request
         self.time_response = None  #TODO: get the lookup time response (figure out if they want it in days, second, milliseconds or what)
         if self.dom_is_ip:
-                self.ip = parsed_url.netloc
+            self.ip = parsed_url.netloc
         else:
             # If the domain is not in IP format find the IP address
             self.ip = ip_record.answer[0] if ip_record.answer else None
 
         # Get info from IP address and WHOIS server stuff
         self.num_resolved_ips = len(ip_record.answer) if ip_record else None
-        obj = ipwhois.IPWhois(self.ip)
+        obj = ipwhois.IPWhois(self.ip) if self.ip else None
         # Store the info from the IPWhois object
 
         # rdap some times returns better  results than whois but most papers use whois data
         # self.dom_record = obj.lookup_rdap()
-        self.dom_record = obj.lookup_whois()  # The only downside (also with rdap) is that we don't get expiration date
-        self.asn_ip = self.dom_record['asn'] if 'asn' in self.dom_record.keys() else None
+        self.dom_record = obj.lookup_whois()  if obj else None# The only downside (also with rdap) is that we don't get expiration date
+        self.asn_ip = self.dom_record['asn'] if obj and ('asn' in self.dom_record.keys()) else None
 
         # TODO: calculate the time in days correctly
         # I think that by "Domain activation time" they mean the time that the domain has been active
         # So we need to get the current date and time and see the difference
-        self.dom_activation_date = self.get_activation_date(self.dom_record)  # this would have to change if we move from whois to rdap
-        self.dom_expiration_date = self.whois_record.expiration_date
+        if self.dom_record:
+            datetime_pattern = re.compile(r'(\d{1,2}/\d{1,2}/\d{4})|(\d{4}-\d{1,2}-\d{1,2})')
+            self.dom_activation_date = self.get_activation_date(self.dom_record)  # this would have to change if we move from whois to rdap
+            self.dom_activation_date = datetime.datetime.strptime(datetime_pattern.search(self.dom_activation_date).group(), '%Y-%m-%d')
+            self.dom_activation_date = datetime.datetime.now() - self.dom_activation_date
+            self.dom_activation_date = self.dom_activation_date.days
+        else:
+            self.dom_activation_date = None
+        self.dom_expiration_date = self.whois_record.expiration_date if type(self.whois_record.expiration_date) != list else self.whois_record.expiration_date[0]
+        if self.dom_expiration_date:
+            self.dom_expiration_date = self.dom_expiration_date - datetime.datetime.now()
+            self.dom_expiration_date = self.dom_expiration_date.days
+
         # self.dom_country = self.dom_record.country
 
         self.domain_dpf = None  # TODO: get mx_servers
@@ -253,12 +270,21 @@ class WebsiteFeatureExtractor:
                          'dir_num_dot', 'dir_num_hyphen', 'dir_num_underline', 'dir_num_slash', 'dir_num_quest', 'dir_num_equ', 'dir_num_at', 'dir_num_amp', 'dir_num_excl', 'dir_num_spaces', 'dir_num_tilde', 'dir_num_comma', 'dir_num_plus', 'dir_num_ast', 'dir_num_pound', 'dir_num_doll', 'dir_num_perc', 'dir_length',
                          'file_num_dot', 'file_num_hyphen', 'file_num_underline', 'file_num_slash', 'file_num_quest', 'file_num_equ', 'file_num_at', 'file_num_amp', 'file_num_excl', 'file_num_spaces', 'file_num_tilde', 'file_num_comma', 'file_num_plus', 'file_num_ast', 'file_num_pound', 'file_num_doll', 'file_num_perc', 'file_length',
                          'params_num_dot', 'params_num_hyphen', 'params_num_underline', 'params_num_slash', 'params_num_quest', 'params_num_equ', 'params_num_at', 'params_num_amp', 'params_num_excl', 'params_num_spaces', 'params_num_tilde', 'params_num_comma', 'params_num_plus', 'params_num_ast', 'params_num_pound', 'params_num_doll', 'params_num_perc', 'params_length', 'params_tld_present', 'params_nums',
-                         'time_response', 'dom_dpf', 'asn_ip', 'dom_activation_date', 'dom_expiration_date', 'num_resolved_ips', 'num_name_servers', 'num_mx_servers', 'ttl', 'tls_ssl_cert', 'num_redirects', 'url_google_indx', 'dom_google_indx', 'url_shortened']
+                         'time_response', 'dom_spf', 'asn_ip', 'dom_activation_date', 'dom_expiration_date', 'num_resolved_ips', 'num_name_servers', 'num_mx_servers', 'ttl', 'tls_ssl_cert', 'num_redirects', 'url_google_indx', 'dom_google_indx', 'url_shortened']
         result = []
         for feature in correct_order:
             current_value = self.__dict__[feature] if self.__dict__[feature] else -1
             result.append(int(current_value))
         return result
+
+    def get_tld(self, str):
+        # returns the number of tld strings in the str variable
+        vectorizer = CountVectorizer(ngram_range=(1, 6), analyzer='char')
+        analyzer = vectorizer.build_analyzer()
+        for _token in analyzer(str):
+            pass
+        # if there are no tlds in the str return None
+        return None
 
 
 
